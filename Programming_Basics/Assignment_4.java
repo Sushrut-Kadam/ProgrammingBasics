8476bd6ff8e7b37ed2c311da3126f70ced5eba3e
Sushrut Kadam <sushrutkadam.123@gmail.com>
2021-06-04 12:00:31 +0530

    Conversion to Western Format

--- Programming_Basics/Assignment_4.java
+++ Programming_Basics/Assignment_4.java
@@ -4,20 +4,26 @@
 //---------------------------------------------------------------------------------------------
 
 class Assignment_4{
-    static String[] table = { 
-                                "tens", 
-                                "hundreds", 
-                                "thoudsands", 
-                                "ten-thousands", 
-                                "lakhs", 
-                                "ten-lakhs", 
-                                "crore" 
-    };
-                        
-    static String[] words = {
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-    };
+    static String[] power = { 
+                        "thoudsands", 
+                        "millions",
+                        "billions",
+                        "trillions",
+                        "quadrillions"
+                    };
 
+    static String[] doubleDigit = {
+                        "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
+                        "ninety"
+    };                        
+
+    static String[] words = {
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+        "eighteen", "nineteen"
+    };
+// ----------------------------------------------------------------------------------------------
+    /*
     static void numberToWords(String number){
         int size = number.length();
 
@@ -27,9 +33,79 @@ class Assignment_4{
         
         System.out.println(words[(((int)number.charAt(size-1))-48)-1]);
     }
+    */
+// -----------------------------------------------------------------------------------------------
 
+    static void westernFormat(String number){
+        int size = number.length();
+        int lastGroup = size % 3;
+        int groups = size/3;
+
+        String index;
+        int position = 0;
+
+        // System.out.println("lastGroup : "+lastGroup);
+        // System.out.println("groups : "+groups);
+
+        if(lastGroup > 0){
+            index = number.substring(0,lastGroup);
+            printGroup(index);
+            System.out.print(power[groups-1]+" ");
+            position += lastGroup;
+        }
+
+        while(groups > 1){
+            index = number.substring(position, position+3);
+            printGroup(index);
+            System.out.print(power[groups-2]+" ");
+            groups--;
+            position = position+3;
+        }
+        index = number.substring(position, position+3);
+        printGroup(index);
+    }
+
+// -----------------------------------------------------------------------------------------------
+
+    static void printGroup(String index){
+        int size = index.length();
+
+        if(size == 1)
+            System.out.print(words[Integer.parseInt(index.substring(0))-1]+" ");
+
+        if(size == 3){
+            int pos1 = Integer.parseInt(index.substring(0,1));
+            System.out.print(words[pos1-1]+" hundred ");
+            size--;
+        }
+
+        if(size == 2){
+            int pos2 = Integer.parseInt(index.substring(1,2));
+
+            if(pos2 <= 1){
+                System.out.print(words[Integer.parseInt(index.substring(1))-1]+" ");
+            }
+            else{
+                System.out.print(doubleDigit[pos2-2]+"-"+words[Integer.parseInt(index.substring(2))-1]+" ");
+            }
+        }  
+    
+    }
+
+// -----------------------------------------------------------------------------------------------
     public static void main(String[] args){
-        String input = "128547";
-        numberToWords(input);
+        String input = "6148371289573447";
+        westernFormat(input);
+        System.out.println();
+        
+        // printGroup("345");
+        // System.out.println();
+        // printGroup("113");
+        // System.out.println();
+        // printGroup("241");
+        // System.out.println();
+        // printGroup("123");
+        // System.out.println();
+        // printGroup("215");
     }
 }
